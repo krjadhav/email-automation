05cbd3f68fdb997b8d8dc743f0f7195aacfcc4a0
krjadhav <j@dhav.me>
2023-06-28 17:29:00 +0530

    add main.py for email processing

---
+++ main.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    # Authenticate to Gmail API
+
+    # Fetch emails from Gmail API
+
+    # Store fetched emails in the database
+
+    # Load rules from JSON file
+
+    # Process emails based on rules
